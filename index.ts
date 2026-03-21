900d028e14f29c5293b7edb44c9a4de902084d06
dantweb <daniil.tkachev@gmail.com>
2026-03-21 21:41:06 +0100

    add l18n to plugins

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import type { IPlugin, IPlatformSDK } from 'vbwd-view-component';
 import { registerCmsVueComponent } from './src/registry/vueComponentRegistry';
 import en from './locales/en.json';
+import de from './locales/de.json';
+import es from './locales/es.json';
+import fr from './locales/fr.json';
+import ja from './locales/ja.json';
+import ru from './locales/ru.json';
+import th from './locales/th.json';
+import zh from './locales/zh.json';
 
 export const cmsPlugin: IPlugin = {
   name: 'cms',
@@ -35,6 +42,13 @@ export const cmsPlugin: IPlugin = {
     });
 
     sdk.addTranslations('en', en);
+    sdk.addTranslations('de', de);
+    sdk.addTranslations('es', es);
+    sdk.addTranslations('fr', fr);
+    sdk.addTranslations('ja', ja);
+    sdk.addTranslations('ru', ru);
+    sdk.addTranslations('th', th);
+    sdk.addTranslations('zh', zh);
   },
 
   activate() { this._active = true; },
